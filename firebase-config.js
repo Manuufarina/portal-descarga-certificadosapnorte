ef9e746ba15792e89893262c2d9e3f1354fb6428
Manuufarina <mfserra@sanisidro.gob.ar>
2025-09-11 09:14:21 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAw0POKFHZkLMJdOvTnqlc4B2jMHaUdC44",
   authDomain: "portal-certificados-antiplaga.firebaseapp.com",
   projectId: "portal-certificados-antiplaga",
-  storageBucket: "portal-certificados-antiplaga.appspot.com",
+  storageBucket: "portal-certificados-antiplaga.firebasestorage.app",
   messagingSenderId: "1027821457649",
   appId: "1:1027821457649:web:4a649763d0505db83c9950"
 };
